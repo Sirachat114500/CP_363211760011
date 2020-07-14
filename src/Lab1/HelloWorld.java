e5cb6399cf35a20fdfeadc3bf4d8c269f5148962
Sirachat <rosebud114500@gmail.com>
2020-07-14 17:55:19 +0700

    add code 2 lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
         System.out.println("Sirachat Pramualsab");
+        System.out.println("Nickname Tian");
+        System.out.println("Age 22");
+        System.out.println("AddressEmail rosebud114500@gmail.com");
+        System.out.println("Telphon 0937159599");
+
+
 
 
     }
