d75979d52f0e709ecc899fdfcbc9cf361675a585
sirachat <rosebud114500@gmail.com>
2020-08-11 15:05:16 +0700

    Lab 2 basic input

--- src/Lab2/Exercise_Lab2_1.java
+++ src/Lab2/Exercise_Lab2_1.java
@@ -10,6 +10,26 @@ public class Exercise_Lab2_1 {
 
         System.out.println("Enter a number 1:");
         int a = scanner.nextInt();
+        System.out.println("Enter a number 2:");
+        int b = scanner.nextInt();
+        System.out.println("Enter a number 3:");
+        int c = scanner.nextInt();
+        System.out.println("Enter a number 4:");
+        int d = scanner.nextInt();
+        System.out.println("Enter a number 5:");
+        int e = scanner.nextInt();
+
+
+        // Finding
+        int total = a + b + c + d + e ;
+        int avg = total / 5;
+
+        //display
+        System.out.println("Total ="+total);
+        System.out.println("Average ="+avg);
+
+
+
 
 
     }
