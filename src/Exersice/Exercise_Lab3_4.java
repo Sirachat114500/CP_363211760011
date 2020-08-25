08c310be4f32b62745fd7e0dc54ff707196b74a6
sirachat <rosebud114500@gmail.com>
2020-08-25 17:08:41 +0700

    Lab 3 :For , Switch_case , While , Do_Wjile

--- src/Exersice/Exercise_Lab3_4.java
+++ src/Exersice/Exercise_Lab3_4.java
@@ -3,23 +3,41 @@ package Exersice;
 import java.util.Scanner;
 
 public class Exercise_Lab3_4 {
+
     public static void main(String[] args) {
-        Scanner SC = new Scanner(System.in);
+
+        //define account
+        String username = "admin";
+        String password = "1234";
+
+        //create Scanner object as sc
+        Scanner sc = new Scanner(System.in);
 
         int count = 0;
-        do {System.out.println("Username:");
-        String u = SC.nextLine();
-        System.out.println("Password:");
-        String P = SC.nextLine()
-        if (u.equals(Username)&& p.equals )
+        do {
+            System.out.print("Username: ");
+            String u = sc.nextLine();
+            System.out.print("Password: ");
+            String p = sc.nextLine();
+            if (u.equals(username) && p.equals(password)) {
+                System.out.println("Welcome to MT System.");
+                break;
+            } else {
+                System.out.println("Username or Password is not correct.");
+                count++;
+                if (count == 3) {
+                    System.out.println("Your account has been locked. Please, contact Admin");
+                }
+            }
+        } while (count < 3);
 
 
 
 
 
 
-        }
-        //define account
+
+
 
 
 
