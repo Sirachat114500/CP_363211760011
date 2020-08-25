7c37d488b4d1fd8a4a3dc9d9019f47d3e408b95c
sirachat <rosebud114500@gmail.com>
2020-08-25 17:00:13 +0700

    Lab 3 :For , Switch_case , While , Do_Wjile

---
+++ src/Exersice/Exercise_Lab3_4.java
@@ -0,0 +1,29 @@
+package Exersice;
+
+import java.util.Scanner;
+
+public class Exercise_Lab3_4 {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+
+        int count = 0;
+        do {System.out.println("Username:");
+        String u = SC.nextLine();
+        System.out.println("Password:");
+        String P = SC.nextLine()
+        if (u.equals(Username)&& p.equals )
+
+
+
+
+
+
+        }
+        //define account
+
+
+
+
+
+    }
+}
