a3790a6795cb101c894d80f76229466669700438
sirachat <rosebud114500@gmail.com>
2020-10-20 16:09:01 +0700

    Lab 3 :For , Switch_case , While , Do_Wjile

---
+++ src/Lab8/ExFinally.java
@@ -0,0 +1,27 @@
+package Lab8;
+
+import java.io.*;
+
+public class ExFinally {
+    public static void main(String[] args) {
+        File myFile = new File("myFile.txt");
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(myFile));
+            String txt = null;
+
+            while ((txt = reader.readLine()) != null) {
+                //display
+                System.out.println(txt);
+            }//while
+            } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Can not read text from file.");
+            e.printStackTrace();
+        }
+
+
+    }//main
+}//class
